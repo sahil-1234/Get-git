b3d327bad602a01d24a533744fb00e6f945966c2
sahil-1234 <96813045+sahil-1234@users.noreply.github.com>
2024-03-02 22:30:40 +0530

    UI Design

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,19 @@
+import Search from "../components/Search";
+import SortRepos from "../components/SortRepos";
+import ProfileInfo from "../components/ProfileInfo";
+import Repos from "../components/Repos";
+const HomePage = () => {
+	return (
+		<div className='m-4'>
+			<Search />
+			<SortRepos />
+			<div className='flex gap-4 flex-col lg:flex-row justify-center items-start'>
+				<ProfileInfo />
+				<Repos />
+			</div>
+		</div>
+	);
+};
+export default HomePage
+
+
