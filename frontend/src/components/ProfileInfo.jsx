84e11be6ae8e089baae27fe88d8d4d1620c0fe26
sahil-1234 <96813045+sahil-1234@users.noreply.github.com>
2024-06-07 08:23:46 +0530

    API integrated

--- frontend/src/components/ProfileInfo.jsx
+++ frontend/src/components/ProfileInfo.jsx
@@ -3,25 +3,14 @@ import { RiGitRepositoryFill, RiUserFollowFill, RiUserFollowLine } from "react-i
 import { FaXTwitter } from "react-icons/fa6";
 import { TfiThought } from "react-icons/tfi";
 import { FaEye } from "react-icons/fa";
+import { formatMemberSince } from "../utils/functions";
 
-const ProfileInfo = () => {
-	const userProfile = {
-		avatar_url: "https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745",
-		bio: "👨🏻‍💻👨🏻‍💻👨🏻‍💻",
-		email: "johndoe@gmail.com",
-		followers: 100,
-		following: 200,
-		html_url: "https://github.com/burakorkmez",
-		location: "Somewhere, Earth",
-		name: "John Doe",
-		public_gists: 100,
-		public_repos: 100,
-		twitter_username: "johndoe",
-		login: "johndoe",
-	};
+const ProfileInfo = ({userProfile}) => {
+
+	const memberSince=formatMemberSince(userProfile?.created_at);
 
 	return (
-		<div className='lg:w-1/3 w-full flex flex-col gap-2 md:sticky md:top-10'>
+		<div className='lg:w-1/3 w-full flex flex-col gap-2 lg:sticky md:top-10'>
 			<div className='bg-glass rounded-lg p-4'>
 				<div className='flex gap-4 items-center'>
 					{/* User Avatar */}
@@ -74,7 +63,7 @@ const ProfileInfo = () => {
 				{/* Member Since Date */}
 				<div className='my-2'>
 					<p className='text-gray-600 font-bold text-sm'>Member since</p>
-					<p className=''>21 Sep, 2023</p>
+					<p className=''>{memberSince}</p>
 				</div>
 
 				{/* Email Address */}
